f56216354acb6bf24d2b55e6d55132c5e5b61bce
Alan Lamas <alan12394@hotmail.com>
2023-01-04 17:17:48 -0300

    added languages

--- app/services/slack_notification_service.rb
+++ app/services/slack_notification_service.rb
@@ -13,9 +13,10 @@ class SlackNotificationService
     'Python': 'python',
     'React': 'react',
     'React-Native': 'react-native',
-    'Dart': 'dart',
-    'Kotlin': 'kotlin',
-    'Swift': 'swift',
+    'Dart': 'flutter',
+    'Objective-C': 'ios',
+    'Kotlin': 'android',
+    'Swift': 'ios',
     'TypeScript': 'typescript',
     'JavaScript': 'javascript'
   }.freeze
