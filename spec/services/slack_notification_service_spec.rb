cdba1f4bcd02f05db50f4bbde25020bd05c33b6b
Cristian Molina <megatux@gmail.com>
2023-01-13 18:14:16 -0300

    More real calls on service test

--- spec/services/slack_notification_service_spec.rb
+++ spec/services/slack_notification_service_spec.rb
@@ -12,31 +12,61 @@ describe SlackNotificationService do
   describe '#send_notification' do
     subject { described_class.new(params).send_notification }
 
+    let(:username) { Faker::Internet.username }
     let!(:user) do
-      User.create(github_name: 'pepe', blacklisted: false)
+      User.create(github_name: username, blacklisted: false)
     end
 
     context 'with valid params' do
+      let(:random_action) { described_class::ACTION_METHODS.keys.sample }
+      let(:channel_name) { described_class::DEFAULT_CHANNEL }
+      let(:some_language) { 'Node' }
       let(:params) do
         {
-          github_webhook: { action: described_class::ACTION_METHODS.keys.sample },
-          channel: '#code-review',
-          pull_request: { user: { login: 'pepe' } }
+          github_webhook: {
+            action: random_action,
+            label: { name: Faker::Internet.slug(words: '_') }
+          },
+          channel: channel_name,
+          pull_request: {
+            user: { login: username },
+            body: Faker::Lorem.sentence
+          },
+          repository: {
+            name: Faker::Internet.slug,
+            language: some_language
+          }
         }
       end
 
       before do
         stub_request(:post, 'https://slack.com/api/conversations.list')
           .to_return(status: 200, body: '', headers: {})
+
+        # stub_request(:post, 'https://slack.com/api/conversations.info')
+        #   .with(body: { 'channel': 'node-code-review' })
+        #   .to_return(status: 200, body: '', headers: {})
+
+        stub_request(:post, 'https://slack.com/api/chat.postMessage')
+          .with(
+            body: {
+              'channel': channel_name,
+              'text': '  :nodejs:',
+              'username': username
+            }
+          ).to_return(status: 200, body: '', headers: {})
       end
 
       it 'sends the message' do
-        expect_any_instance_of(described_class).to receive(:send).and_return(nil)
+        expect_any_instance_of(described_class)
+          .to receive(:send)
+          .with(described_class::ACTION_METHODS[random_action])
+          .and_call_original
         subject
       end
 
       context 'with a PR from a blacklisted user' do
-        let!(:user) { User.create(github_name: 'pepe', blacklisted: true) }
+        let!(:user) { User.create(github_name: username, blacklisted: true) }
 
         it 'does NOT send the message' do
           expect_any_instance_of(described_class).not_to receive(:send)
